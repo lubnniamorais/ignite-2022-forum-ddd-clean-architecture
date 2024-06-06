7aeff80579fda9396167f9fec12f529fdf86c401
Lubnnia <lubnnia@gmail.com>
2024-06-06 16:43:55 -0300

    feat: Create slug value object

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,28 @@
+export class Slug {
+  public value: string;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  /**
+   * Recebe um texto e normaliza como um slug
+   *
+   * Exemplo: "An example title" => "an-example-title"
+   *
+   * @param text {string}
+   */
+  static createFromText(text: string) {
+    const slugText = text
+      .normalize('NFKD')
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '');
+
+    return new Slug(slugText);
+  }
+}
