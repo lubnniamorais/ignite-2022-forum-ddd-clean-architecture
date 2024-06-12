92b82a96747825b94cb43eef8e508c250fa9af25
Lubnnia <lubnnia@gmail.com>
2024-06-12 10:59:54 -0300

    feat: Create factory make-question and use case get-question-by-slug

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -5,7 +5,7 @@ import { AnswerQuestionUseCase } from './answer-question';
 let inMemoryAnswersRepository: InMemoryAnswersRepository;
 let sut: AnswerQuestionUseCase;
 
-describe('Create answer', () => {
+describe('Create Answer', () => {
   beforeEach(() => {
     inMemoryAnswersRepository = new InMemoryAnswersRepository();
     sut = new AnswerQuestionUseCase(inMemoryAnswersRepository);
