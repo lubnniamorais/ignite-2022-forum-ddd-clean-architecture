52c8e83a4cc080c63c7891104bdafcd5654f3a6b
Lubnnia <lubnnia@gmail.com>
2024-06-15 15:16:29 -0300

    feat: Create use case edit answer

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -3,5 +3,6 @@ import { Answer } from '@/domain/forum/enterprise/entities/answer';
 export interface AnswersRepository {
   create(answer: Answer): Promise<void>;
   findByID(id: string): Promise<Answer | null>;
+  save(answer: Answer): Promise<void>;
   delete(answer: Answer): Promise<void>;
 }
